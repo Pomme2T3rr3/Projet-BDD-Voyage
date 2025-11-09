7033d27494644a953afda09282a72c40aa23c946
sweakz <enfantmtg@gmail.com>
2025-11-09 23:56:12 +0100

    Correction erreurs

--- projet-voyage.sql
+++ projet-voyage.sql
@@ -14,31 +14,31 @@ DROP TABLE IF EXISTS parle CASCADE;
 
 
 
-CREATE TABLE agence{
+CREATE TABLE agence( 
     idA serial primary key,
     nom varchar(25),
     adresse text
-};
+);
 
-CREATE TABLE pays{
+CREATE TABLE pays(
     codeP char(3) primary key,
     nom varchar(25)
-};
+);
 
-CREATE TABLE ville{
+CREATE TABLE ville(
     idVille serial primary key,
     nom varchar(20),
     pays char(3) references pays(codeP),
     descriptif text
-};
+);
 
-CREATE TABLE visa{
+CREATE TABLE visa(
     idVisa serial primary key,
     typev varchar(10),
     prixVisa int,
-};
+);
 
-CREATE TABLE client{
+CREATE TABLE client(
     idCli serial primary key,
     nom varchar(20),
     prenom varchar(15),
@@ -48,9 +48,9 @@ CREATE TABLE client{
     adr varchar(30),
     numtel varchar(13),
     courriel varchar(25)
-};
+);
 
-CREATE TABLE employe{
+CREATE TABLE employe(
     idEmp serial primary key,
     nom varchar(20),
     prenom varchar(15),
@@ -58,9 +58,9 @@ CREATE TABLE employe{
     mdp varchar(8),
     Travaille int references agence(idA),
     est_resp int references agence(idA)
-};
+);
 
-CREATE TABLE etape{
+CREATE TABLE etape( 
     idEt serial primary key,
     TyP varchar(20),
     transport varchar(15),
@@ -68,43 +68,43 @@ CREATE TABLE etape{
     arrivee int idVille references ville(idVille),
     dateDepart date NOT NULL,
     dateArrivee date NOT NULL
-};
+);
 
-CREATE TABLE voyage{
+CREATE TABLE voyage(
     idVoy serial primary key,
     dateDebut date ,
     dateFin date ,
     PrixPersonne numeric(6,2),
     descriptif text,
     planifie_par int references employe(idEmp)
-};
+);
 /*
  --- Creation de tables des associations ---
 */
-CREATE TABLE fait{
+CREATE TABLE fait(
     idCli int references client(idCli),
     idVoy int references voyage(idVoy),
     primary key(idCli, idVoy)
-};
+);
 
-CREATE TABLE constitue{
+CREATE TABLE constitue(
     idVoy int references voyage(idVoy),
     idEt int references etape(idEt),
     primary key(idVoy, idEt)
-};
+);
 
-CREATE TABLE obtient{
+CREATE TABLE obtient(
     idCli int references client(idCli),
     codeP char(3) references pays(codeP),
     idVisa int references visa(idVisa),
     primary key(idCli, codeP , idVisa)
-};
+);
 
-CREATE TABLE parle{
+CREATE TABLE parle(
     codeP int references pays(codeP),
     langue varchar(15),
     primary key(idCli, idVoy)
-};
+);
 
 
 -- TABLE agence
