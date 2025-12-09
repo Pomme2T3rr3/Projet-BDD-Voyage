b104efaa52154d70495ff9bde0e99a836a97ad4e
Pomme2T3rr3 <damian.deneuville@gmail.com>
2025-12-09 15:45:22 +0100

    main updated + consigne_rendu + client.html

---
+++ consignes_rendu.md
@@ -0,0 +1,5 @@
+Evalués particulièrement sur (rendre Dimanche soir):
+
+fonctionnalités techniques apportées
+pistes d'amélioration
+organiosation du travail
--- main.py
+++ main.py
@@ -14,5 +14,30 @@ def accueil():
     return render_template("accueil.html")
 
 
+# pas fonctionnel
+@app.route("/client", methods=["GET", "POST"])
+def manage_client():
+    if request.method == "POST":
+        nom = request.form["nom"]
+        prenom = request.form["prenom"]
+
+        with conn.cursor() as cur:
+            cur.execute(
+                """
+                INSERT INTO client (nom, prenom, sex, age, nat, adr, numtel, courriel)
+                VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
+                """,
+                (nom, prenom),
+            )
+
+        return redirect(url_for("manage_client"))
+
+    with conn.cursor() as cur:
+        cur.execute("SELECT * FROM client")
+        client = cur.fetchall()
+
+    return render_template("client.html", client=client)
+
+
 if __name__ == "__main__":
     app.run(debug=True)
