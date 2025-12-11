088e9eb084b8e427470b478da2175575cffd1e5d
sweakz <enfantmtg@gmail.com>
2025-12-11 01:58:06 +0100

    ajout templates espace_client/pro + ajout fctions (main.py): connexion, verification

--- main.py
+++ main.py
@@ -13,6 +13,43 @@ conn = db.connect()
 def accueil():
     return render_template("accueil.html")
 
+# Page de connexion
+@app.route("/profil")
+def connexion():
+    return render_template("connexion_profil.html") 
+
+# Pas fonctionnel pour l'instant
+@app.route("/verification", methods=["GET", "POST"])
+def verification():
+    lst_client = []
+    lst_employe = []
+    
+    login = request.form.get["num_login"]
+    mdp = request.form.get["mdp"]
+
+    if not login or not mdp:
+        return redirect(url_for("connexion_profile.html"))
+
+    with conn.cursor() as cur:
+        cur.execute("SELECT Clogin, Cmdp FROM client;")
+        for i in cur.fetchall():
+            lst_client.append([i[0],i[1]])
+        cur.close()
+    
+    with conn.cursor() as cur:
+        cur.execute("SELECT sitelogin, mdp FROM employe;")
+        for i in cur.fetchall():
+            lst_employe.append([i[0],i[1]])
+        cur.close()
+
+    if [login, mdp] in lst_client:
+        return render_template("espace_client.html")
+    
+    elif [login, mdp] in lst_employe:
+        return render_template("espace_pro.html")
+    else : 
+        return redirect(url_for("connexion_profile.html"))
+
 
 # pas fonctionnel
 @app.route("/client", methods=["GET", "POST"])
